058ccae99624ed20b40290a88b2b930557d5611c
Kevin Adam <33047790+vnxx@users.noreply.github.com>
2020-04-30 14:11:09 +0700

    Update images.js

--- src/images.js
+++ src/images.js
@@ -2,16 +2,16 @@ export const images = [
   {
     name: "gambar 1",
     url:
-      "https://pempro.test.mti-tech.id/storage/dokumentasi/5dZsPCuJ55o07tYVGzN8f6upZGHoLYDj8V7tSGbR.jpeg"
+      "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iqAMzxFRr2a0/v1/1000x-1.jpg"
   },
   {
     name: "gambar 2",
     url:
-      "https://pempro.test.mti-tech.id/storage/dokumentasi/wibW3sSh2L6D9kFAgFSp0yhawddkLeHbCZl29Dwj.jpeg"
+      "https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
   },
   {
     name: "gambar 3",
     url:
-      "https://pempro.test.mti-tech.id/storage/dokumentasi/ZQCiOJc7Q7eWfgli4zi6QCLNFptsMwaak4E8JLTs.jpeg"
+      "https://fivmagazine.com/wp-content/uploads/2019/07/beitragsbild-foto-shooting-model-commercial-fashion.jpg"
   }
 ];
